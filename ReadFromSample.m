8e6bdf5a2013a92afd7b5e62602b73dbae94de41
unknown <green.koopa667@gmail.com>
2017-09-15 16:14:24 -0400

    Removing the coordinate class because it's pretty pointless

--- ReadFromSample.m
+++ ReadFromSample.m
@@ -1,4 +1,4 @@
-function [ coords ] = ReadFromSample( filename )
+function [ x, y ] = ReadFromSample( filename )
 % Reads from data that is formatted the same way the given sample data is:
 % i.e. with Name, type, ... Dimension, NODE_COORD_SECTION.
 % This assumes Coordinates will begin displaying at line 8
@@ -15,9 +15,8 @@ i = 1;
 while ~feof(f)
     line = fgetl(f);
     c = strsplit(line);
-    x = single(str2double(c(2)));
-    y = single(str2double(c(3)));
-    coords(i) = Coordinate(x,y);
+    x(i) = single(str2double(c(2)));
+    y(i) = single(str2double(c(3)));
     i = i + 1;
 end
 fclose(f);
