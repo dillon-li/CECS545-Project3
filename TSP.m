a777ae1e44c4b92f6f1666bcf7c0961097fb801d
unknown <green.koopa667@gmail.com>
2017-09-18 18:39:09 -0400

    New distance formula. Also created node table. Seems to mostly work

--- TSP.m
+++ TSP.m
@@ -1,20 +1,25 @@
-function [ EdgeLords ] = TSP( filename )
+function [ EdgeLords, x, y ] = TSP( filename )
 % Main Traveling Salesman Problem
 % Project 3 Style: Closest Edge Insertion Heuristic:
-
+ 
 % Input: Filename
-
+ 
 % Output:
-
+ 
 % Read in Coordinates
 [x,y] = ReadFromSample(filename);
-
+ 
 % Array of cities not in the current tour (initially cities 2:end)
 unLoved = zeros(size(x,2)-1,1);
 for i = 1:size(unLoved,1)
     unLoved(i) = i+1;
 end
 
+% Node Table
+NodeTable = table();
+for i = 1:size(x,2)
+    NodeTable(i,1) = num2cell(i);
+end
 % Initialization: Begin with a tour of 3 cities
 currentTour = [1];
 currentCity = 1;
@@ -40,35 +45,45 @@ city3 = city3 + 1;
 unLoved(city2) = [];
 indexToRemove = find(unLoved==city3);
 unLoved(indexToRemove) = [];
+
+%[wG, g] = MakeGraph(EdgeLords,x,y);
+% PlotPoints(x,y);
+count = 3;
     
 % Loop until all cities are loved (added to the tour)
-while size(unLoved,1) ~= 0
-    pointToEdge = inf; % Distance from point to line segment
-    
-    % Pick arbitrary next city
-    currentCity = unLoved(1);
-    unLoved(1) = [];
-    
-    % Find the edgiest of current edges
+while size(unLoved,2) ~= 0
+    edgiest = inf; % Distance from point to line segment    
+    % Find the closest edge-city pair
     for k = 1:size(EdgeLords,2)
-        cStart = EdgeLords(1,k); % Starting city of line segment
-        cEnd = EdgeLords(2,k); % Ending city of line segment
-        coordStart = [x(cStart) y(cStart) 0];
-        coordEnd = [x(cEnd) y(cEnd) 0];
-        coordCity = [x(closeCity) y(closeCity) 0];
-        d = point_to_line(coordCity, coordStart, coordEnd);
-        if d < pointToEdge
-            pointToEdge = d; % Smallest distance from segment to new city
-            edgiest = k; % Index of closest edge
+        for j = 1:size(unLoved,1)
+            currentCity = unLoved(j);
+            cStart = EdgeLords(1,k); % Starting city of line segment
+            cEnd = EdgeLords(2,k); % Ending city of line segment
+            dist1 = Distance(x(cStart), y(cStart), x(currentCity), y(currentCity));
+            dist2 = Distance(x(currentCity), y(currentCity), x(cEnd), y(cEnd));
+            dist3 = Distance(x(cStart), y(cStart), x(cEnd), y(cEnd));
+            d = dist1 + dist2 - dist3;
+            if d < edgiest
+                edgiest = d; % Smallest distance from segment to new city
+                edgeIndex = k; % Index of closest edge in pair
+                cityIndex = j; % Index of closest city in pair
+            end
         end
     end
     % Add the city in between the closest edge
-    temp = EdgeLords(2,k); % Intermediate node
-    EdgeLords(2,k) = currentCity; % Add first part of new edges
-    newEdge2 = [currentCity; temp]; % Add second part of new edges
+    temp = EdgeLords(2, edgeIndex); % Intermediate node
+    EdgeLords(2,edgeIndex) = unLoved(cityIndex); % Add first part of new edges
+    newEdge2 = [unLoved(cityIndex); temp]; % Add second part of new edges
     EdgeLords = [EdgeLords newEdge2];
+    g = MakeGraph(EdgeLords,x,y,NodeTable);
+    count = count + 1;
+    %hold on;
+    plot(g,'XData',x,'YData',y);
+    hold off;
+    unLoved(cityIndex) = [];
     
 end
-
+ 
 end
+ 
 
